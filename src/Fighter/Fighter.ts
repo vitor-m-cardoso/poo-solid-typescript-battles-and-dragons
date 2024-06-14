66318ade8028c1d367de1244f0374944da0d07cf
Vitor Cardoso <vitor.mauricioc@outlook.com>
2024-06-14 11:25:16 -0300

    adiciona interface `Fighter` e `SimpleFighter`

---
+++ src/Fighter/Fighter.ts
@@ -0,0 +1,14 @@
+import Energy from '../Energy';
+import SimpleFighter from './SimpleFighter';
+
+export default interface Fighter {
+  lifePoints: number;
+  strength: number;
+  defense: number;
+  energy?: Energy;
+
+  attack(enemy: Fighter | SimpleFighter): void;
+  special?(enemy: Fighter): void;
+  levelUp(): void;
+  receiveDamage(attackPoints: number): number;
+}
